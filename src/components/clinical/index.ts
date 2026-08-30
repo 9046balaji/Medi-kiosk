bf11229d294058677c21ecd3df2dcf9478aafc66
Konda Balaji Rao <balajikonda9046@gmail.com>
2026-08-30 09:39:50 +0530

    feat(clinical): add index.ts clinical workstation component

---
+++ src/components/clinical/index.ts
@@ -0,0 +1,5 @@
+export { DoctorDashboardScreen } from './doctor/DoctorDashboardScreen';
+export { NurseConsoleScreen } from './nurse/NurseConsoleScreen';
+export { ShiftHandoffModal } from './nurse/ShiftHandoffModal';
+export { FhirExportScreen } from './export/FhirExportScreen';
+export { SessionPurgeModal } from './export/SessionPurgeModal';
